ef0a44cb1122d3af2ead8c553b196c3d86a3f08c
josefernandez <jose.fernandez.wa@gmail.com>
2016-10-29 19:11:51 -0700

    schematest-java: removed test non-functional plugins

--- schemast-java/schemast/src/main/java/com/schemast/schemas/SchemaBuilder.java
+++ schemast-java/schemast/src/main/java/com/schemast/schemas/SchemaBuilder.java
@@ -1,30 +1,14 @@
 package com.schemast.schemas;
 
 import com.schemast.plugins.json.JsonSchemaParser;
-import com.schemast.plugins.logs.LogSchemaParser;
-import com.schemast.plugins.properties.PropertiesSchemaParser;
-import com.schemast.plugins.xsd.XsdSchemaParser;
-import com.schemast.plugins.yml.YmlSchemaParser;
 
 public class SchemaBuilder {
     private SchemaParser jsp = new JsonSchemaParser();
-    private SchemaParser xsp = new XsdSchemaParser();
-    private SchemaParser lsp = new LogSchemaParser();
-    private SchemaParser ysp = new YmlSchemaParser();
-    private SchemaParser psp = new PropertiesSchemaParser();
 
     public Schema parse(String type, String schema) {
         switch (type) {
             case "json":
                 return validate(jsp.parse(schema));
-            case "xsd":
-                return validate(xsp.parse(schema));
-            case "log":
-                return validate(lsp.parse(schema));
-            case "yml":
-                return validate(ysp.parse(schema));
-            case "properties":
-                return validate(psp.parse(schema));
             default:
                 throw new UnknownSchemaTypeException(type);
         }
